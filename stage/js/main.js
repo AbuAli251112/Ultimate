c8fa2cd321177c78dd9a9eb1658f4fcba885e2ef
AbuAli251121 <abualininja@gmail.com>
2022-09-19 01:49:30 +0200

    Fourth Commit

--- stage/js/main.js
+++ stage/js/main.js
@@ -6,3 +6,21 @@ document.querySelector(".toggle-sidebar").onclick = function () {
   document.querySelector(".content-area").classList.toggle("no-sidebar");
   document.querySelector(".sidebar").classList.toggle("no-sidebar");
 };
+
+document.querySelectorAll(".toggle-submenu").forEach((i) => {
+  i.addEventListener("click", function () {
+    if (this.lastChild.classList.contains("fa-angle-right")) {
+      this.lastChild.classList.remove("fa-angle-right");
+
+      this.lastChild.classList.add("fa-angle-down");
+
+      this.nextElementSibling.style.display = "block";
+    } else {
+      this.lastChild.classList.remove("fa-angle-down");
+
+      this.lastChild.classList.add("fa-angle-right");
+
+      this.nextElementSibling.style.display = "none";
+    }
+  });
+});
