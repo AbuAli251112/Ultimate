d572659f207174f6e34f108cbb63089746d8af06
AbuAli251121 <abualininja@gmail.com>
2022-10-06 03:03:15 +0200

    Last Commit

--- stage/js/main.js
+++ stage/js/main.js
@@ -25,8 +25,109 @@ document.querySelectorAll(".toggle-submenu").forEach((i) => {
   });
 });
 
-document.querySelector(".dropdown").onclick = function (e) {
+document.querySelector(".dropdown").onclick = function () {
 
   this.lastElementChild.classList.toggle("toggler");
   
 };
+
+document.querySelector("#dropdown").onclick = function () {
+
+  this.parentElement.nextElementSibling.classList.toggle("toggler");
+
+}
+
+document.querySelector('.toggle-fullscreen').onclick = function () {
+
+  this.classList.toggle('full-screen');
+
+  if (this.classList.contains('full-screen')) {
+
+    openFullScreen();
+
+  } else {
+
+    closeFullScreen();
+
+  }
+
+};
+
+var elem = document.documentElement;
+
+function openFullScreen() {
+
+  if (elem.requestFullscreen) {
+
+    elem.requestFullscreen();
+
+  } else if (elem.mozRequestFullScreen) {
+
+    elem.mozRequestFullScreen();
+
+  } else if (elem.webkitRequestFullScreent) {
+
+    lem.webkitRequestFullScreent();
+
+  } else if (elem.msRequestFullScreen) {
+
+    elem.msRequestFullScreen();
+
+  }
+
+}
+
+function closeFullScreen() {
+
+  if (document.exitFullscreen) {
+
+    document.exitFullscreen();
+
+  } else if (document.mozCancelFullScreen) {
+
+    document.mozCancelFullScreen();
+
+  } else if (document.webkitExitFullScreent) {
+
+    document.webkitExitFullScreent();
+
+  } else if (document.msExitFullScreen) {
+
+    document.msExitFullScreen();
+
+  }
+
+}
+
+document.querySelector(".toggle-settings").onclick = function () {
+
+  this.firstElementChild.classList.toggle('fa-spin');
+  this.parentElement.classList.toggle("hide-settings");
+
+;}
+
+let themesClasses = [];
+
+let colors = Array.from(document.querySelectorAll('.color-options li'));
+
+colors.forEach((color) => {
+
+  themesClasses.push(color.getAttribute('data-theme'));
+
+  color.addEventListener('click', function () {
+
+    Array.from(this.parentElement.children).forEach(li => {
+
+      li.classList.remove('active');
+
+    });
+
+    this.classList.add('active');
+
+    document.querySelector("body").classList.remove(...themesClasses);
+
+    document.querySelector("body").classList.add(this.getAttribute('data-theme'));
+
+  });
+
+});
